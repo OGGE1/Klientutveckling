78919c00a51a9f2dac8b7fddf3e4c56934b2239a
Oscar Norman <oscar.norman95@gmail.com>
2021-03-15 17:06:19 +0100

    Shopping list

---
+++ Lektioner/Lektion 5/Övning - Inköpslista/JS/JavaScript.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  $("#button").click(function () {
+    let article = $("#userInput").val();
+
+    if (article.trim() == "") {
+      alert("Ange en artikel!");
+      $("#userInput").val("");
+    } else {
+      $("#tbodyTable").prepend(
+        "<tr>" +
+          "<td>" +
+          "<div class='row'>" +
+          "<div class='col-1'><input class='form-check-input' type='checkbox' value='' id='checkbox'> </div>" +
+          "<div class='col-9'> " +
+          article +
+          "</div>" +
+          "<div class='col-2'>" +
+          "<button class='btn btn-primary btn-sm' id='removeButton'>Ta bort</button>" +
+          "</div>" +
+          "</div>" +
+          "</td>" +
+          "</tr>"
+      );
+      $("#userInput").val("");
+    }
+
+    $("#removeButton").click(function () {
+      $(this).closest("tr").remove();
+    });
+
+    $("#checkbox").click(function () {
+      if ($(this).is(":checked")) {
+        $(this).closest("tr").css("background-color", "#00FF00");
+      } else {
+        $(this).closest("tr").css("background-color", "");
+      }
+    });
+  });
+});
